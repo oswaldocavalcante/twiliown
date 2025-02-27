ca7ec71e30085854acd57cdc0fd4d2a962761f96
Oswaldo Cavalcante <jocmail@gmail.com>
2025-02-27 17:08:34 -0300

    Refactoring

--- public/assets/js/twn-public.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function ($)
-{
-    $(document).on('cwg_instock_after_email_field', function () {
-        $('.cwgstock_email')
-            .val('naoresponda@ryanne.com.br')
-            .prop('readonly', true)
-            .closest('.form-group')
-    });
-});
