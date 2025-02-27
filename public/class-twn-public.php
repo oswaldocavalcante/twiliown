cb55af3a62463ae61479396d11163167ecb55afa
Oswaldo Cavalcante <jocmail@gmail.com>
2025-02-27 17:45:54 -0300

    Fix: hiding email field for all users.

--- public/class-twn-public.php
+++ public/class-twn-public.php
@@ -4,15 +4,18 @@ class TWN_Public
 {
     public function hide_email_field()
     {
-        ?>
-        <script>
-            jQuery(document).ready(function($) {
-                $('input[name="cwgstock_email"]')
-                    .val('dontsend@email.com')
-                    .prop('readonly', true)
-                    .hide();
-            });
-        </script>
-        <?php
+        if (current_user_can('manage_options'))
+        {
+            ?>
+            <script>
+                jQuery(document).ready(function($) {
+                    $('input[name="cwgstock_email"]')
+                        .val('dontsend@email.com')
+                        .prop('readonly', true)
+                        .hide();
+                });
+            </script>
+            <?php
+        }
     }
 }
